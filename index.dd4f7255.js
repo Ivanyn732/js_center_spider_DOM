b892294f89709f0d37fdc92001891bc68ff19ebd
Ivan Dovzhanyn <ivan.dovzhanyn@gmail.com>
2024-09-27 20:08:05 +0300

    make build

---
+++ index.dd4f7255.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.getBoundingClientRect(),n=t.getBoundingClientRect(),i=c.height,l=c.width,o=n.height,d=n.width;t.style.left="".concat(l/2-d/2,"px"),t.style.top="".concat(i/2-o/2,"px");
+//# sourceMappingURL=index.dd4f7255.js.map
